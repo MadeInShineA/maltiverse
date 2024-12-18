b3b312e6aba479968fb1cc4783e1ef49abfef674
GDbateaux <yolan.savioz@hes-so.ch>
2024-12-18 15:34:59 +0100

    Add questions

---
+++ queries.sql
@@ -0,0 +1,16 @@
+/*Quels festivals se déroulent dans une ville spécifique ?*/
+
+
+/*Quel est le visiteur ayant écrit le plus d'avis sur les bières ?*/
+
+
+/*Quels types de bières ont une moyenne d'alcool supérieure à 5% ?*/
+
+
+/*Quels sont les bars ayant une note moyenne supérieure à 4 ?*/
+
+
+/*Quels sont les bars ayant une note moyenne supérieure à 4 ?*/
+
+
+/*Quel est la bière avec le meilleur avis ?*/
