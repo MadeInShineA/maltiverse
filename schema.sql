591b683a361d03b7e34edbd786f333d36a319661
NathanAnto <nathan4567@outlook.com>
2024-12-18 15:54:50 +0100

    Removed back ticks and fixed primary keys

--- schema.sql
+++ schema.sql
@@ -1,117 +1,118 @@
-CREATE TABLE `festivals` (
-  `id` integer PRIMARY KEY,
-  `name` varchar(255),
-  `start_date` timestamp,
-  `end_date` timestamp,
-  `city_id` integer
+-- Active: 1729717232443@@localhost@5432@matliverse@public
+CREATE TABLE festivals (
+  id serial primary key,
+  name varchar(255),
+  start_date timestamp,
+  end_date timestamp,
+  city_id integer
 );
 
-CREATE TABLE `visitors` (
-  `id` integer PRIMARY KEY,
-  `name` varchar(255),
-  `surname` varchar(255),
-  `sex` boolean,
-  `birth_date` date
+CREATE TABLE visitors (
+  id serial primary key,
+  name varchar(255),
+  surname varchar(255),
+  sex boolean,
+  birth_date date
 );
 
-CREATE TABLE `bars` (
-  `id` integer PRIMARY KEY,
-  `name` varchar(255),
-  `address` varchar(255),
+CREATE TABLE bars (
+  id serial primary key,
+  name varchar(255),
+  address varchar(255),
+  festival_id integer
 );
 
-CREATE TABLE `beers` (
-  `id` integer,
-  `name` varchar(255),
-  `type_id` integer,
-  `brand_id` integer,
-  `alcool_percentage` float
+CREATE TABLE beers (
+  id serial primary key,
+  name varchar(255),
+  type_id integer,
+  brand_id integer,
+  alcool_percentage float
 );
 
-CREATE TABLE `beer_types` (
-  `id` integer,
-  `name` varchar(255)
+CREATE TABLE beer_types (
+  id serial primary key,
+  name varchar(255)
 );
 
-CREATE TABLE `beer_brands` (
-  `id` integer,
-  `name` varchar(255)
+CREATE TABLE beer_brands (
+  id serial primary key,
+  name varchar(255)
 );
 
-CREATE TABLE `visitor_beer_reviews` (
-  `id` integer,
-  `created_at` timestamp,
-  `content` varchar(255),
-  `rating` integer,
-  `beer_id` integer,
-  `visitor_id` integer
+CREATE TABLE visitor_beer_reviews (
+  id serial primary key,
+  content varchar(255),
+  rating integer,
+  beer_id integer,
+  visitor_id integer
 );
 
-CREATE TABLE `visitor_bar_reviews` (
-  `id` integer,
-  `created_at` timestamp,
-  `content` varchar(255),
-  `rating` integer,
-  `bar_id` integer,
-  `visitor_id` integer
+CREATE TABLE visitor_bar_reviews (
+  id serial primary key,
+  content varchar(255),
+  rating integer,
+  bar_id integer,
+  visitor_id integer
 );
 
-CREATE TABLE `visitor_festival_reviews` (
-  `id` integer,
-  `created_at` timestamp,
-  `content` varchar(255),
-  `rating` integer,
-  `festival_id` integer,
-  `visitor_id` integer
+CREATE TABLE visitor_festival_reviews (
+  id serial primary key,
+  content varchar(255),
+  rating integer,
+  festival_id integer,
+  visitor_id integer
 );
 
-CREATE TABLE `relation_bar_festival` (
-  `bar_id` integer,
-  `festival_id` integer
+CREATE TABLE relation_bar_festival (
+  bar_id integer,
+  festival_id integer,
+  constraint pk_relation_bar_festival primary key (bar_id, festival_id)
 );
 
-CREATE TABLE `relation_beer_bar` (
-  `bar_id` integer,
-  `beer_id` integer,
-  `price` float,
-  `quantity` integer
+CREATE TABLE relation_beer_bar (
+  bar_id integer,
+  beer_id integer,
+  price float,
+  quantity integer,
+  constraint pk_relation_beer_bar primary key (beer_id, bar_id)
 );
 
-CREATE TABLE `cities` (
-  `id` integer PRIMARY KEY,
-  `name` varchar(255),
-  `country_id` integer
+CREATE TABLE cities (
+  id serial primary key,
+  name varchar(255),
+  country_id integer
 );
 
-CREATE TABLE `countries` (
-  `id` integer PRIMARY KEY,
-  `name` varchar(255)
+CREATE TABLE countries (
+  id serial primary key,
+  name varchar(255)
 );
 
-ALTER TABLE `relation_bar_festival` ADD FOREIGN KEY (`bar_id`) REFERENCES `bars` (`id`);
+ALTER TABLE relation_bar_festival ADD FOREIGN KEY (bar_id) REFERENCES bars (id);
 
-ALTER TABLE `relation_bar_festival` ADD FOREIGN KEY (`festival_id`) REFERENCES `festivals` (`id`);
+ALTER TABLE relation_bar_festival ADD FOREIGN KEY (festival_id) REFERENCES festivals (id);
 
-ALTER TABLE `beer` ADD FOREIGN KEY (`brand_id`) REFERENCES `beer_brands` (`id`);
+ALTER TABLE beers ADD FOREIGN KEY (brand_id) REFERENCES beer_brands (id);
 
-ALTER TABLE `visitor_bar_reviews` ADD FOREIGN KEY (`bar_id`) REFERENCES `bars` (`id`);
+ALTER TABLE visitor_bar_reviews ADD FOREIGN KEY (bar_id) REFERENCES bars (id);
 
-ALTER TABLE `visitor_bar_reviews` ADD FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`);
+ALTER TABLE visitor_bar_reviews ADD FOREIGN KEY (visitor_id) REFERENCES visitors (id);
 
-ALTER TABLE `relation_beer_bar` ADD FOREIGN KEY (`bar_id`) REFERENCES `bars` (`id`);
+ALTER TABLE relation_beer_bar ADD FOREIGN KEY (bar_id) REFERENCES bars (id);
 
-ALTER TABLE `relation_beer_bar` ADD FOREIGN KEY (`beer_id`) REFERENCES `beers` (`id`);
+ALTER TABLE relation_beer_bar ADD FOREIGN KEY (beer_id) REFERENCES beers (id);
 
-ALTER TABLE `beers` ADD FOREIGN KEY (`type_id`) REFERENCES `beer_types` (`id`);
+ALTER TABLE beers ADD FOREIGN KEY (type_id) REFERENCES beer_types (id);
 
-ALTER TABLE `visitor_festival_reviews` ADD FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`);
+ALTER TABLE visitor_festival_reviews ADD FOREIGN KEY (visitor_id) REFERENCES visitors (id);
 
-ALTER TABLE `visitor_festival_reviews` ADD FOREIGN KEY (`festival_id`) REFERENCES `festivals` (`id`);
+ALTER TABLE visitor_festival_reviews ADD FOREIGN KEY (festival_id) REFERENCES festivals (id);
 
-ALTER TABLE `visitor_beer_reviews` ADD FOREIGN KEY (`beer_id`) REFERENCES `beers` (`id`);
+ALTER TABLE visitor_beer_reviews ADD FOREIGN KEY (beer_id) REFERENCES beers (id);
 
-ALTER TABLE `visitor_beer_reviews` ADD FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`);
+ALTER TABLE visitor_beer_reviews ADD FOREIGN KEY (visitor_id) REFERENCES visitors (id);
 
-ALTER TABLE `cities` ADD FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`);
+ALTER TABLE cities ADD FOREIGN KEY (country_id) REFERENCES countries (id);
 
-ALTER TABLE `festivals` ADD FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`);
+ALTER TABLE festivals ADD FOREIGN KEY (city_id) REFERENCES cities (id);
