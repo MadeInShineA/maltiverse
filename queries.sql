e315373b5f8ef0b7512b8bf9b8d0100461056338
GDbateaux <111980428+GDbateaux@users.noreply.github.com>
2024-12-18 15:44:01 +0100

    questions pull request by @GDbateaux
    * Add questions
    
    * Add \n
    
    * fix language

---
+++ queries.sql
@@ -0,0 +1,13 @@
+/*Which festivals take place in a specific city?*/
+
+
+/*Which visitor has written the most reviews about beers?*/
+
+
+/*Which types of beers have an average alcohol content higher than 5%?*/
+
+
+/*Which bars have an average rating higher than 4?*/
+
+
+/*Which beer has the best review?*/
